1401fffa13db0c4ea2137fa545e526c519b7a1ef
Auke-Dirk Pietersma <aukedirk@horus.nu>
2024-03-10 16:13:36 +0100

    update

--- testbench/byteblast8/byteblast8_tb.v
+++ testbench/byteblast8/byteblast8_tb.v
@@ -2,12 +2,18 @@
 
 module fde_tb();
 
+
 reg clk;
 reg enable;
 reg load;
 reg reset;
-reg [7:0]nxt_adr;
-wire [7:0]crnt_adr;
+reg w_ram;
+reg [5:0]nxt_adr;
+
+reg[7:0]ram_reg;
+wire [5:0]crnt_adr;
+wire [7:0]value_out;
+
 
 wire fetch;
 wire decode;
@@ -24,9 +30,7 @@ end
   [fde:fetch] -> [pc:enable]
 */
 
-
-
-pc pc1(
+pc #(6) pc1(
     .clk(clk),
     .enable(fetch),
     .reset(reset),
@@ -43,21 +47,38 @@ fde fde1(
     .execute(execute)
     );
 
+ram ram1(
+    .clk(clk),
+    .enable(w_ram), // should be called w_enable
+    .address(crnt_adr),
+    .data_in(ram_reg),
+    .data_out(value_out)
+    );
+ 
   integer i;
   initial 
   begin
+    ram1.data[0] = 1;
+    ram1.data[1] = 2;
+    ram1.data[2] = 3;
+    ram1.data[3] = 4;
+
     #1
     $dumpfile("vcd/byteblast8.vcd");    
     $display("<begin>");
-
+    #1
+    $monitor("%b> %b %b %b: %d %d",clk, fetch,decode, execute, crnt_adr,value_out);
+    #1
+    $display("<----->");
+    #1
     for (i=0; i<12; i=i+1) begin
       #1
       clk = 1;
-      $monitor("%b %b %b: %d",fetch,decode, execute, crnt_adr);
+      $monitor("%b> %b %b %b: %d %d",clk, fetch,decode, execute, crnt_adr,value_out);
 
       #1
       clk = 0;
-      $monitor("%b %b %b: %d",fetch,decode, execute,crnt_adr);
+      $monitor("%b> %b %b %b: %d %d",clk, fetch,decode, execute,crnt_adr,value_out);
     end
 
 
